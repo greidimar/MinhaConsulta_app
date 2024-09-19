1c02c7b53db3e96df2c1d6a4ade345e9999b04d1
greidimar <greidi123@gmail.com>
2024-09-19 20:13:18 -0300

    Parte 3

--- App.tsx
+++ App.tsx
@@ -1,16 +1,14 @@
-import { Box, Center, NativeBaseProvider, Text } from 'native-base';
+import { NavigationContainer } from '@react-navigation/native';
+import { NativeBaseProvider } from 'native-base';
 import React from 'react';
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Center flex={1} bg="white">
-        <Box>
-          <Text fontSize="2xl" color="primary.500" fontWeight="bold" fontFamily="Roboto">
-            Olá, nosso app está configurado com Native Base!
-          </Text>
-        </Box>
-      </Center>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
     </NativeBaseProvider>
   );
 }
