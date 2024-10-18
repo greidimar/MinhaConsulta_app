12c7f1d9eba2d61b839da112ff025e0ed5e7db81
greidimar <greidi123@gmail.com>
2024-10-17 23:44:49 -0300

    Aula 26/09/2024 - Criando o Backend para o Projeto MinhaConsulta - Implementação no Frontend - Aplicativo MinhaConsulta_app Parte 2

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,34 +1,66 @@
+import React, { useState } from 'react';
+import { NativeBaseProvider, Box, Button, Input, Center } from 'native-base';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { Box, Button, Center, Input, NativeBaseProvider } from 'native-base';
-import React from 'react';
-import { RootStackParamList } from '../navigation/AppNavigator'; // Importação da tipagem correta
+import { RootStackParamList } from '../navigation/AppNavigator';
+import { Alert as RNAlert } from 'react-native'; // Importar o Alert do React Native
+import { login } from '../api/auth'; // Importe a função de login que você já criou
 
 type LoginScreenNavigationProp = NativeStackNavigationProp<
-    RootStackParamList,
-    'Login'
+  RootStackParamList,
+  'Login'
 >;
 
 type Props = {
-    navigation: LoginScreenNavigationProp;
+  navigation: LoginScreenNavigationProp;
 };
 
 const LoginScreen = ({ navigation }: Props) => {
-    return (
-        <NativeBaseProvider>
-            <Center flex={1} bg="white">
-                <Box>
-                    <Input placeholder="Email" mb={4} />
-                    <Input placeholder="Senha" mb={4} secureTextEntry />
-                    <Button onPress={() => navigation.navigate('ConsultationsList')}>
-                        Entrar
-                    </Button>
-                    <Button onPress={() => navigation.navigate('SignUp')} mt={4}>
-                        Cadastrar
-                    </Button>
-                </Box>
-            </Center>
-        </NativeBaseProvider>
-    );
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleLogin = async () => {
+    try {
+      const token = await login(username, password);
+      console.log('Token:', token);
+      RNAlert.alert('Login realizado com sucesso!');
+      navigation.navigate('ConsultationsList');
+    } catch (err: unknown) {
+      // Verificar se err é uma instância de Error
+      if (err instanceof Error) {
+        RNAlert.alert('Erro', err.message);
+      } else {
+        RNAlert.alert('Erro', 'Ocorreu um erro inesperado.');
+      }
+    }
+  };
+
+  return (
+    <NativeBaseProvider>
+      <Center flex={1} bg="white">
+        <Box>
+          <Input
+            placeholder="Usuário"
+            mb={4}
+            value={username}
+            onChangeText={setUsername}
+          />
+          <Input
+            placeholder="Senha"
+            mb={4}
+            secureTextEntry
+            value={password}
+            onChangeText={setPassword}
+          />
+          <Button onPress={handleLogin}>
+            Entrar
+          </Button>
+          <Button onPress={() => navigation.navigate('SignUp')} mt={4}>
+            Cadastrar
+          </Button>
+        </Box>
+      </Center>
+    </NativeBaseProvider>
+  );
 };
 
 export default LoginScreen;
